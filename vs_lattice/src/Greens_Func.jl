273f2b190834b51e06918a82830bd6bed80607d9
Jiwoo <92397624+jiwoop@users.noreply.github.com>
2023-09-12 23:21:56 -0500

    Add files via upload

---
+++ vs_lattice/src/Greens_Func.jl
@@ -0,0 +1,33 @@
+# By Vivian.
+
+# two ways of calculating the retarded green's function in code! 
+# (I like the phrase "time-retarded" but the internet may be more helpful without saying "time")
+
+# 1: Immediately spit out Gʳ::Matrix{ComplexF64} values of a specific Hamiltonian::Matrix{ComplexF64} at a specific k vector and energy
+# 2: Or, after defining a H::Function (k::Vector{Float64}) in a given scope, can define Gʳ::Function as a function of ω and k. 
+
+# The latter is more useful if you get in the "functional" mindset. 
+# it makes more sense in my head but this might be infurating idk
+
+# APPROACH 1:
+function GʳofH(H::Union{Matrix{ComplexF64},Matrix{Float64}}, ω::Union{Float64, ComplexF64})
+	return inv(ω*I - im*H)
+end
+
+# APPROACH 2:
+# Okay this is a cool feature of julia
+# We will define a function that spits out a function!
+# Ultimately, this will return a function Gʳ(k,ω) "the retarded green's function"
+function genGʳ(H::Function)
+	function Gʳ(ω::Union{Float64, ComplexF64}, k::Vector{Float64})
+		return inv(ω*I - im*H(k))
+	end
+	return Gʳ
+end
+
+function genGʳ_nok(H::Function)
+	function Gʳ(ω::Union{Float64, ComplexF64})
+		return inv(ω*I - im*H(0.0))
+	end
+	return Gʳ
+end
---
+++ vs_lattice/src/Whole_Shebang.jl
@@ -0,0 +1,39 @@
+# we want to write a code to, when given a unit cell with a certain set of parameters, 
+# perform all of these interesting analyses!
+include("DW_Position.jl")
+include("Plot.jl")
+include("Unitcell.jl")
+
+function wholeAnalysis(system::DWLattice, Ncells::Int, race_i::Int, race_j::Int, NNs::Int, x::Function, f₀::Float64, name::String)
+    # Make directory for the unit lattice if doesn't exist.
+    if(!isdir("vs_lattice/img/" * name))
+        mkpath("vs_lattice/img/" * name)
+    end
+
+    # Magic Numbers
+    Δt = 1/f₀
+    ω = 2*π/f₀    # angular freq.
+    step = 1
+    Npts = 100
+    ϵ = 0.001
+
+    # Position y(t)
+    y = DWPosition(system, Ncells, race_i, race_j, x, f₀)
+    # Transfer Function T
+    T = transferFunc(system, Ncells, race_i, race_j, 0.0, 0.0, step, NNs=NNs, eval=false)
+
+    # get the band structure and DOS of the periodic structure
+    getBands(system, NNs, true, title=name)
+
+    # now get the Bode Plot regarding the transfer function of one site on another
+    BodePlot(T, ϵ*GHz, 25*GHz, Npts, title=name)
+
+    # now convolve the impulse response with the input DW pos x(t) to get y(t)
+    Plot_TwoFs(x, y, Δt, title=name)
+end
+
+f₀ = 18*GHz/(3*2*π)
+wholeAnalysis(AFM2R_lattice, 10, 10, 7, 200, genFiring_neuron_DWpos(f₀), f₀, "AFM2R_Triangle")
+#wholeAnalysis(FM1R_lattice, 12, 8, 7, 200, genFiring_neuron_DWpos(f₀), f₀, "FM1R_Triangle_7")
+#wholeAnalysis(FM1R_lattice, 12, 8, 6, 200, genFiring_neuron_DWpos(f₀), f₀, "FM1R_Triangle_6")
+#wholeAnalysis(FM1R_lattice, 12, 8, 5, 200, genFiring_neuron_DWpos(f₀), f₀, "FM1R_Triangle_5")
