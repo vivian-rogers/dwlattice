ae3d88be13fa42eab7674d8c26907d80dbeb2941
vivian-rogers <43078448+vivian-rogers@users.noreply.github.com>
2024-03-17 17:06:43 -0500

    Update crystal_v2.py

--- crystal_v2.py
+++ crystal_v2.py
@@ -1,27 +1,35 @@
+import numpy as np
+
 class Material:
     def __init__(self, vector1, vector2, vector3, coordinates, names):
-        self.a1 = vector1  # Basis vector a
-        self.a2 = vector2  # Basis vector b
-        self.a3 = vector3  # Basis vector c
-        self.atomic_positions = coordinates
+        self.A = vector1  # Basis vector a1, a2, a3 as columns of the matrix A
+        self.atomic_positions = coordinates # coordinates in units of crystal basis vectors
         self.atomic_names = names
 
 def genAtomicPositions(layer_numbers, materials):
     all_positions = []  # blank list
-
-    for material in materials:  # iterate thru each material
-        for i in range(len(material.atomic_names)):  # iterate thru each atom in each material
-            name = material.atomic_names[i]
-            p, q, s = material.atomic_positions[i]  # get coordinates by index
-
-            # r = pa1 + qa2 + sa3
-            r = [
-                p * material.a1[0] + q * material.a2[0] + s * material.a3[0],
-                p * material.a1[1] + q * material.a2[1] + s * material.a3[1],
-                p * material.a1[2] + q * material.a2[2] + s * material.a3[2]
-            ]
-            all_positions.append({"Name": name, "Position": r})  # add atom to list
-
+    total_number_layers = sum(layer_numbers)
+    start_z = 0
+    for material_index in materials:  # iterate thru each material
+        material=materials[material_index]
+        
+        # I will now do a manipulation to strain this layer according to the 0th layer in the stack
+        volume_material = np.det(material.A) # gives the volume of the unit cell
+        strain_x = 1 - np.norm(material[0].A[:,1])/np.norm(material[material_index].A[:,1])
+        strain_y = 1 - np.norm(material[0].A[:,2])/np.norm(material[material_index].A[:,2])
+        print("Strain in x = " + strain_x*100 + " %, strain in y = ")
+        # now, we will fix the other two 
+        material[material_index].A[:,1] = material[0].A[:,1]
+        material[material_index].A[:,1] = material[0].A[:,1]
+        
+        for layer_index in range(0, layer_numbers[material_index]-1):
+            for i in range(len(material.atomic_names)):  # iterate thru each atom in each material
+                name = material.atomic_names[i]
+                Ri_relative = material.atomic_positions[i]  # get relative coordinates R of the ith atom
+                # r = pa1 + qa2 + sa3
+                Ri_absolute = material.A*Ri_relative + start_z*[0, 0, 1]
+                all_positions.append({"Name": name, "Position": Ri_absolute})  # add atom to list
+            start_z += A[2,2] # add on the z component of the crystal lattice basis vector to stack up the supercell in z
     return all_positions
 
 # Example stack of Iron and Silicon Nitride
